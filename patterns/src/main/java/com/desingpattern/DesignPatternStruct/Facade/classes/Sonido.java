d7c887b0199d9e704a7648de2c107e6362a78861
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-03-30 20:10:44 -0500

    Implement Facade pattern with CineFacade class and related components for movie management

---
+++ patterns/src/main/java/com/desingpattern/DesignPatternStruct/Facade/classes/Sonido.java
@@ -0,0 +1,6 @@
+package com.desingpattern.DesignPatternStruct.Facade.classes;
+
+public class Sonido {
+    public void subirSonido(){System.out.println("se ha subido el volumen!");}
+    public void bajarSonido(){System.out.println("se ha bajado el sonido!");}
+}
