43e50a1f4dd61175f236acd6f8764f24f0174e84
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-03-31 10:43:35 -0500

    Refactor Prototype pattern demonstration to use cloning for vehicle instances

--- patterns/src/main/java/com/desingpattern/DesignPatternCreations/Prototype/Main.java
+++ patterns/src/main/java/com/desingpattern/DesignPatternCreations/Prototype/Main.java
@@ -3,8 +3,8 @@ package com.desingpattern.DesignPatternCreations.Prototype;
 public class Main {
     public static void main(String[] args) {
         Vehiculo vehiculo = new Vehiculo("cherolevt", "201");
-        Vehiculo vehiculo2 = new Vehiculo("cherolevt", "2021");
-
+        Vehiculo vehiculo2 = vehiculo.clone();
+        
         System.out.println(vehiculo == vehiculo2);
     }
 }
